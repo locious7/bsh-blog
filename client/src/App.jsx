625c7eb8eb5711dbc11fd5c4cb73f0e63afdb6d8
locious <locious7@protonmail.com>
2024-05-30 17:16:28 -0600

    add header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,10 +5,12 @@ import Dashboard from './pages/Dashboard';
 import Projects from './pages/Projects';
 import Home from './pages/Home';
 import About from './pages/About';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
