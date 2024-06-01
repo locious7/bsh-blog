b0262e87169e32a30ffa99eeb56a8f65d6196a1f
locious <locious7@protonmail.com>
2024-05-31 21:30:40 -0600

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log('Connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
+
 
 const app = express();
-
 
 app.listen(3000, () => {
   console.log('Server started on port 3000!');
