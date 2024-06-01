f28b7df978c6ba480a39d24858264950a8eae71a
locious <locious7@protonmail.com>
2024-06-01 15:19:57 -0600

    create test api route

--- api/index.js
+++ api/index.js
@@ -1,23 +1,24 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
 })
-.then(() => {
-  console.log('Connected to MongoDB');
-})
-.catch((err) => {
-  console.error('Error connecting to MongoDB:', err);
-});
-
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.error("Error connecting to MongoDB:", err);
+    });
 
 const app = express();
 
 app.listen(3000, () => {
-  console.log('Server started on port 3000!');
+    console.log("Server started on port 3000!");
 });
+
+app.use("/api/user", userRoutes);
+
