b96f2c773a8de2d421e42305bac102bfe01f8119
locious <locious7@protonmail.com>
2024-06-05 19:03:39 -0600

    add footer componenet

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Projects from './pages/Projects';
 import Home from './pages/Home';
 import About from './pages/About';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
   )
 }
---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,88 @@
+import { Footer } from "flowbite-react"
+import { Link } from "react-router-dom"
+import { BsInstagram, BsYoutube, BsFacebook, BsTwitter } from "react-icons/bs"
+
+export default function FooterComp() {
+    return (
+        <Footer container className="border border-t-8 border-yellow-500 bg-white dark:bg-gray-900 dark:border-gray-700 ">
+            <div className="w-full max-w-7xl mx-auto">
+                <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+                    <div className="mt-5">
+                        <Link
+                            to="/"
+                            className="self-center whitespace-nowrap text-2xl sm:text-xl font-semibold dark:text-white"
+                        >
+                            <span className="px-2 py-1 bg-gradient-to-r from-green-500 to-yellow-400 rounded-lg text-white">
+                                Beestings and Honey Blog
+                            </span>
+                        </Link>
+                    </div>
+                    <div className="grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+                        <div>
+                            <Footer.Title title="About" />
+                            <Footer.LinkGroup col>
+                                <Footer.Link
+                                    href="/contact"
+                                    target="_blank"
+                                    rel="noopener noreferrrr"
+                                >
+                                    Contact
+                                </Footer.Link>
+                                <Footer.Link
+                                    href="/about"
+                                    target="_blank"
+                                    rel="noopener noreferrrr"
+                                >
+                                    About BSH Blog
+                                </Footer.Link>
+                            </Footer.LinkGroup>
+                        </div>
+                        <div>
+                            <Footer.Title title="Follow US" />
+                            <Footer.LinkGroup col>
+                                <Footer.Link
+                                    href="https://www.youtube.com/@beestingsandhoney1"
+                                    target="_blank"
+                                    rel="noopener noreferrrr"
+                                >
+                                    YouTube
+                                </Footer.Link>
+                                <Footer.Link
+                                    href="https://www.instagram.com/"
+                                    target="_blank"
+                                    rel="noopener noreferrrr"
+                                >
+                                    Instagram
+                                </Footer.Link>
+                            </Footer.LinkGroup>
+                        </div>
+                        <div>
+                            <Footer.Title title="Legal" />
+                            <Footer.LinkGroup col>
+                                <Footer.Link
+                                    href="#"
+                                >
+                                    Privacy Policy
+                                </Footer.Link>
+                                <Footer.Link
+                                    href="#"
+                                >
+                                    Terms and Conditions
+                                </Footer.Link>
+                            </Footer.LinkGroup>
+                        </div>
+                    </div>
+                </div>
+                <Footer.Divider className="mt-8" />
+                <div className="w-full sm:flex sm:items-center sm:justify-between">
+                    <Footer.Copyright href='#' by="Beestings and Honey Blog" year={new Date().getFullYear()} />
+                    <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+                        <Footer.Icon href="#" target="_blank" rel="noopener noreferrrr" icon={BsFacebook}  />
+                        <Footer.Icon href="#" target="_blank" rel="noopener noreferrrr" icon={BsInstagram}  />
+                        <Footer.Icon href="https://www.youtube.com/@beestingsandhoney1" target="_blank" rel="noopener noreferrrr" icon={BsYoutube}  />
+                    </div>
+                </div>
+            </div>
+        </Footer>
+    )
+}
