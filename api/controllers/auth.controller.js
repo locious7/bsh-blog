b669f164ed0c72c25b5d7ad327b454deb3fff993
locious <locious7@protonmail.com>
2024-06-15 11:12:43 -0600

    add update user api route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,23 @@
 import User from "../models/user.model.js";
-import brcyptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
-    if (!username || !email || !password || username === '' || email === '' || password === '') {
-        next(errorHandler(400, 'All fields are required'))
+    if (
+        !username ||
+        !email ||
+        !password ||
+        username === "" ||
+        email === "" ||
+        password === ""
+    ) {
+        next(errorHandler(400, "All fields are required"));
     }
 
-    const hashedPassword = brcyptjs.hashSync(password, 10);
+    const hashedPassword = bcryptjs.hashSync(password, 10);
 
     const newUser = new User({
         username,
@@ -20,43 +27,51 @@ export const signup = async (req, res, next) => {
 
     try {
         await newUser.save();
-        res.json('Signup Successful');
+        res.json("Signup Successful");
     } catch (error) {
         next(error);
     }
 };
 
-
 export const signin = async (req, res, next) => {
     const { email, password } = req.body;
 
-    if (!email || !password || email === '' || password === '') {
-        next(errorHandler(400, 'All fields are required'))
+    if (!email || !password || email === "" || password === "") {
+        next(errorHandler(400, "All fields are required"));
     }
 
     try {
         const validUser = await User.findOne({ email });
 
         if (!validUser) {
-            return next(errorHandler(404, 'User not found'));
+            return next(errorHandler(404, "User not found"));
         }
 
         const isPasswordValid = brcyptjs.compareSync(password, validUser.password);
 
         if (!isPasswordValid) {
-            return next(errorHandler(400, 'Invalid password'));
+            return next(errorHandler(400, "Invalid password"));
         }
 
         const token = jwt.sign(
-            { userId: validUser._id }, process.env.JWT_SECRET, { expiresIn: '1d' }
-        )
+            { userId: validUser._id },
+            process.env.JWT_SECRET //*, { expiresIn: '1d' }*//*
+        );
 
-        const { password: pass, _id: id, createdAt: created, updatedAt: updated, __v: v, ...rest } = validUser._doc;
+        const {
+            password: pass,
+            createdAt: created,
+            updatedAt: updated,
+            __v: v,
+            ...rest
+        } = validUser._doc;
 
-        res.status(200).cookie('access_token', token, {
-            httpOnly: true,
-        }).json(rest);
-
+        res
+            .status(200)
+            .cookie("access_token", token, {
+                httpOnly: true,
+            })
+            .json(rest);
     } catch (error) {
         next(error);
     }
@@ -68,17 +83,30 @@ export const google = async (req, res, next) => {
     try {
         const user = await User.findOne({ email });
         if (user) {
-            const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1d' });
-            const { password, ...rest } = user._doc;
-            res.status(200).cookie('access_token', token, {
-                httpOnly: true,
-            }).json(rest);
+            const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+                expiresIn: "1d",
+            });
+            const {
+                password: pass,
+                createdAt: created,
+                updatedAt: updated,
+                __v: v,
+                ...rest
+            } = user._doc;
+            res
+                .status(200)
+                .cookie("access_token", token, {
+                    httpOnly: true,
+                })
+                .json(rest);
         } else {
-            const gerneratedPassword = Math.random().toString(36).slice(-8) + Math.random().toString(36).slice(-8);
+            const gerneratedPassword =
+                Math.random().toString(36).slice(-8) +
+                Math.random().toString(36).slice(-8);
             const hashedPassword = brcyptjs.hashSync(gerneratedPassword, 10);
             const newUser = new User({
                 username:
-                    name.toLowerCase().split(' ').join('') +
+                    name.toLowerCase().split(" ").join("") +
                     Math.random().toString(9).slice(-8),
                 email,
                 password: hashedPassword,
@@ -89,10 +117,11 @@ export const google = async (req, res, next) => {
                 { id: newUser._id, isAdmin: newUser.isAdmin },
                 process.env.JWT_SECRET
             );
-            const { password, _id, createdAt, updatedAt, __v, ...rest } = newUser._doc;
+            const { password, _id, createdAt, updatedAt, __v, ...rest } =
+                newUser._doc;
             res
                 .status(200)
-                .cookie('access_token', token, {
+                .cookie("access_token", token, {
                     httpOnly: true,
                 })
                 .json(rest);
